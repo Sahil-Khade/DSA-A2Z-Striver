6a3bbfc9d1cdc5ac774092d0b2ed35f0f33c2120
Sahil <sk.sahilkhade@gmail.com>
2026-06-15 22:31:15 +0530

    Arrays Easy: Largest Element (O(n))

---
+++ Step_03_Arrays/Easy/06_Largest_Element.cpp
@@ -0,0 +1,29 @@
+// Problem: Largest Element in Array
+// Link: https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1
+// Platform: GeeksforGeeks
+// Difficulty: Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largest(vector<int> &arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+};
+
+/*
+APPROACH:
+Initialize max with first element.
+Iterate through array, update max if larger element found.
+
+TIME: O(n)
+SPACE: O(1)
+*/
