afd281eb5667ebf03e47a6006e1889b32fc69819
Sahil <sk.sahilkhade@gmail.com>
2026-06-16 22:07:52 +0530

    Pattern 2

---
+++ Step_01_Learn_the_Basics/Patterns/2.cpp
@@ -0,0 +1,26 @@
+// Input: 5
+// Output:
+// *
+// **
+// ***
+// ****
+// *****
+
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    pattern(n);
+    return 0;
+}
