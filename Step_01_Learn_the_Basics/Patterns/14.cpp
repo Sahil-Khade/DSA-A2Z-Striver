4d0e800768e6ed2538d9484e13cee6eee35c6316
Sahil <sk.sahilkhade@gmail.com>
2026-06-16 22:44:59 +0530

    Pattern 14

---
+++ Step_01_Learn_the_Basics/Patterns/14.cpp
@@ -0,0 +1,26 @@
+// Input: 5
+// Output:
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
+
+#include<iostream>
+using namespace std;
+
+void pattern(int rows){
+    for(int i = 1; i <= rows; i++){
+        for(int j = 0; j < i; j++){
+            cout << char('A'+j);
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    pattern(n);
+    return 0;
+}
