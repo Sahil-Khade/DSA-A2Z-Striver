6e6705ec2be7bca76376b99a68e08a3171b7328a
Sahil <sk.sahilkhade@gmail.com>
2026-06-16 22:57:44 +0530

    Pattern 19-20

---
+++ Step_01_Learn_the_Basics/Patterns/20.cpp
@@ -0,0 +1,49 @@
+// Input: 5
+// Output:
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+#include<iostream>
+using namespace std;
+
+void pattern(int rows){
+    for(int i = 1; i <= rows; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*"; 
+        }
+        for(int space = 1; space <= 2*(rows-i); space++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << "*"; 
+        }
+        cout << endl;
+    }
+    
+    for(int i = 1; i < rows; i++){
+        for(int j = i; j < rows; j++){
+            cout << "*"; 
+        }
+        for(int space = 1; space <= 2*i; space++){
+            cout << " ";
+        }
+        for(int j = i; j < rows; j++){
+            cout << "*"; 
+        }
+        cout << endl;
+    }    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    pattern(n);
+    return 0;
+}
