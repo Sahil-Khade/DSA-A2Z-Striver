037b98ee640cf50d981ed452aa6d3ebb983b3508
Sahil <sk.sahilkhade@gmail.com>
2026-06-18 20:36:51 +0530

    Add Right Rotation

--- Step_03_Arrays/Easy/05_Right_Rotate_Array.cpp
+++ Step_03_Arrays/Easy/05_Right_Rotate_Array.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
      int n = nums.size();
-     k = k%n;                         //for k > size of array  
+     k = k%n;                               //handle k > n 
      reverse(nums.begin(),nums.end());
      reverse(nums.begin(),nums.begin()+k);
      reverse(nums.begin()+k,nums.end());
