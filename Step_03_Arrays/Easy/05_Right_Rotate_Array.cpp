25ce0722207be70e885703ec2cbf857a25eb630f
Sahil <sk.sahilkhade@gmail.com>
2026-06-18 20:13:05 +0530

    Add Right Rotation Array

---
+++ Step_03_Arrays/Easy/05_Right_Rotate_Array.cpp
@@ -0,0 +1,22 @@
+// Problem: Rotate Array (LeetCode) 
+// Link: https://leetcode.com/problems/rotate-array/
+// Right Rotation
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+     int n = nums.size();
+     k = k%n;                         //for k > size of array  
+     reverse(nums.begin(),nums.end());
+     reverse(nums.begin(),nums.begin()+k);
+     reverse(nums.begin()+k,nums.end());
+    }
+};
+
+// Approach: reverse whole, reverse first k, reverse rest
+// Example: nums=[1,2,3,4,5,6,7], k=3
+// 1 whole [7,6,5,4,3,2,1]
+// 2 first k [5,6,7,4,3,2,1]
+// 3 rest [5,6,7,1,2,3,4]
+// Time: O(n)
+// Space: O(1)
